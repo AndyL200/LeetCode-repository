6b90b01184cabbb69c419f2ef7cebeedb9c51b69
Andrew <aleclair.1105@outlook.com>
2024-07-11 21:35:46 -0500

    29 beats 90 percent lets go

--- Problem/29.py
+++ Problem/29.py
@@ -1,22 +1,32 @@
+class Solution(object):
+    def divide(self, dividend, divisor):
+        Innercount = 1
+        Outercount = 0
+        orgDiv = abs(divisor)
+        truthyDivend = False if dividend < 0 else True
+        truthyDivsor = False if divisor < 0 else True
+        dividend = abs(dividend)
+        divisor = abs(divisor)
+        while divisor < dividend:
+            pastDiv = divisor
+            pastCount = Innercount
+            divisor += divisor
+            Innercount += Innercount
+            if divisor > dividend:
+                dividend -= pastDiv
+                divisor = orgDiv
+                Outercount += pastCount
+                Innercount = 1
 
-def divide(dividend, divisor):
-    count = 1
-    orgDiv = divisor
-    orgDivend = dividend
-    truthyDivend = False if dividend < 0 else True
-    truthyDivsor = False if divisor < 0 else True
-    dividend = abs(dividend)
-    divisor = abs(divisor)
-    while dividend > 0:
-        dividend = dividend - divisor
-        divisor = divisor + divisor
-        count = count + count
+        dividend -= divisor
+        Outercount = Outercount + Innercount if dividend == 0 else Outercount
 
-    count = count - divide(abs(dividend), abs(orgDiv)) if abs(orgDivend) > abs(dividend) else count
-    
-    if(truthyDivend == truthyDivsor):
-        return count
-    else:
-        return -count
-
-print(divide(1000,10))
+        if Outercount > 2147483647 and truthyDivend == truthyDivsor:
+            return 2147483647
+        elif Outercount > 2147483648 and truthyDivend != truthyDivsor:
+            return -2147483648
+        else:
+            if truthyDivend == truthyDivsor:
+                return Outercount
+            else:
+                return -Outercount
--- Road to 75/443.py
+++ Road to 75/443.py
@@ -1,14 +1,23 @@
 def compress(self, chars):
-    arr = 0
-    for i in range(len(chars)):
-        if(chars[i] == chars[i+1]):
-            j = i
-            while(j < len(chars)):
-                if chars[i] == chars[j]:
+    i=0
+    while i < len(chars):
+        arr = 1
+        if(i < len(chars)-1 and chars[i] == chars[i+1]):
+            j=len(chars)-1
+            while chars.count(chars[i]) > 1:
+                if chars[j] == chars[i]:
                     arr += 1
-            chars.remove(chars[i])
-            for c in str(arr):
-                chars.append(c)
+                    chars.pop(j)
+                j -= 1
 
-print(compress("", ["a","b","b","b","b","b","b","b","b","b","b","b","b"]))
+            count = 1
+            for c in str(arr):
+                chars.insert(i + count, c)
+                count += 1
+        i += 1
+    return chars
+        
+            
+
+print(compress("", ["a","b","b","b","b","b","b",'c','c','t','t','y']))
     
